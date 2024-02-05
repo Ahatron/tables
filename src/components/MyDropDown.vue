df5e59b3efdf96e3fe4af8634cd074c618dcc346
Ahatron <ahatpolatov@gmail.com>
2024-02-05 21:55:32 +0600

    ладно

--- src/components/MyDropDown.vue
+++ src/components/MyDropDown.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    <div class="d-inline-flex align-items-stretch w-100 my-dropdown">
-      <input type="text" class="rounded-start border-end-0 p-2 w-100" />
+  <div class="my-dropdown">
+    <div
+      class="d-inline-flex align-items-stretch w-100 my-dropdown__input"
+      @click.stop="dropper = !dropper"
+    >
+      <input
+        @input="findValues"
+        :value="selectedValue"
+        type="text"
+        class="rounded-start border-end-0 p-2 w-100"
+      />
       <button class="rounded-end w-auto border-start-0" style="background-color: #f6f5f3">
         <svg
-          style="fill: #c0c0c0"
+          :class="dropper ? 'rotate' : ''"
+          style="fill: #c0c0c0; transition: 0.2s"
           xmlns="http://www.w3.org/2000/svg"
           height="24"
           viewBox="0 -960 960 960"
@@ -14,21 +23,70 @@
         </svg>
       </button>
     </div>
-    <div class="rounded shadow"></div>
+
+    <li v-if="dropper" @click.stop type="none" class="rounded shadow my-dropdown__list">
+      <ul
+        v-for="item of matchedValues"
+        :key="item"
+        @click="selectedValue = item"
+        class="my-dropdown__list-item"
+      >
+        {{
+          item
+        }}
+      </ul>
+    </li>
   </div>
 </template>
 
 <script setup lang="ts">
-//
+import { ref } from 'vue'
+
+const dropper = ref(false),
+  values = ref([
+    'Мраморный щебень фр. 2-5 мм, 25кг',
+    'Мраморный щебень фр. 2-5 мм, 25кг (белый)',
+    'Мраморный щебень фр. 2-5 мм, 25кг (вайт)'
+  ]),
+  selectedValue = ref(''),
+  matchedValues = ref(values.value)
+
+function findValues() {
+  if (selectedValue.value) {
+    matchedValues.value = values.value.filter((item: string) =>
+      item.startsWith(selectedValue.value)
+    )
+  } else {
+    matchedValues.value = values.value
+  }
+}
+
+document.addEventListener('click', () => (dropper.value = false))
 </script>
 
 <style scoped>
-.my-dropdown > input,
-button {
-  border: 1px solid #ccc;
+.rotate {
+  transform: rotateZ(90deg);
+}
+.my-dropdown__list-item {
+  margin: 0;
+  padding: 10px 10px;
+}
+.my-dropdown__list-item:hover {
+  background-color: #f6f5f3;
+}
+.my-dropdown__list-item:active {
+  background-color: #d4d4d4;
 }
 
-input {
-  outline-width: 1px !important;
+.my-dropdown__list::marker {
+  marker: none;
+}
+.my-dropdown__input:focus {
+  outline: 1px black solid;
+}
+.my-dropdown__input > input,
+button {
+  border: 2px solid #ccc;
 }
 </style>
